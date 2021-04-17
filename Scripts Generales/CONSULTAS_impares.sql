aa2c56a258d7f0cbe92abd66b2921110756cc56b
Gary <gortiz1490@gmail.com>
2021-04-16 22:55:23 -0600

    update scriptbck y fragmentacion

--- Scripts Generales/CONSULTAS_impares.sql
+++ Scripts Generales/CONSULTAS_impares.sql
@@ -5,13 +5,14 @@ WHERE CASOS.ID_PAIS = PAIS.ID_PAIS AND CASOS.ID_PAIS=14 ORDER BY CASOS.DATEE ASC
 	CREATE INDEX index_id_pais ON PAIS(ID_PAIS, LOCATIONN);
 	DROP INDEX index_id_pais ON PAIS;
 /*CONSULTA 1 : 1.
-Consulta que agrupe la cantidad actual de contagios por paÃ­s.*/
+Consulta que agrupe la cantidad actual de contagios por país.*/
+
 
 	SELECT CASOS.ID_PAIS, PAIS.LOCATIONN , CASOS.TOTAL_CASES , CASOS.DATEE FROM CASOS, PAIS
 	WHERE CASOS.ID_PAIS = PAIS.ID_PAIS AND CASOS.DATEE= DATEADD(day,-3,CONVERT(date,GETDATE()));
 	select DATEADD(day,-4,CONVERT(date,GETDATE()));
 
-/*CONSULTA 3 : Consulta que agrupe la cantidad actual de contagios de los Ãºltimos 3 meses por continente*/
+/*CONSULTA 3 : Consulta que agrupe la cantidad actual de contagios de los últimos 3 meses por continente*/
 	SELECT T2.ID_CONTINENTE,T2.Con1, (T2.total_ayer-T1.total_3meses)Total_casos_3MESES_hasta_hoy FROM(
 	SELECT CONTINENTE.ID_CONTINENTE, (CONTINENTE.CONTINENT)Con ,SUM(CASOS.TOTAL_CASES)total_3meses FROM CASOS, PAIS, CONTINENTE
 	WHERE CASOS.ID_PAIS = PAIS.ID_PAIS AND PAIS.ID_CONTINENTE=CONTINENTE.ID_CONTINENTE 
@@ -40,15 +41,18 @@ Consulta que agrupe la cantidad actual de contagios por paÃ­s.*/
 	AND
 	CASOS.DATEE BETWEEN '2020-03-01' AND '2020-06-30'
 	GROUP BY CASOS.ID_PAIS,PAIS.LOCATIONN;
+
 /*POR CONTINENTE*/
 	SELECT CONTINENTE.ID_CONTINENTE, (CONTINENTE.CONTINENT)Con, AVG(ISNULL(CASOS.NEW_CASES,0))PROMEDIO FROM CASOS, PAIS, CONTINENTE
 	WHERE CASOS.ID_PAIS=PAIS.ID_PAIS AND PAIS.ID_CONTINENTE=CONTINENTE.ID_CONTINENTE
 	AND
 	CASOS.DATEE BETWEEN '2020-03-01' AND '2020-06-30'
 	GROUP BY CONTINENTE.CONTINENT, CONTINENTE.ID_CONTINENTE;
+/*A nivel mundial*/
+	SELECT AVG(ISNULL(CASOS.NEW_CASES,0))PROMEDIO_MUNDIAL FROM CASOS
+	WHERE CASOS.DATEE BETWEEN '2020-03-01' AND '2020-06-30'
 
-
-/*CONSULTA 7: Crear una vista que muestre al top 10 de paÃ­ses con mayor cantidad de pruebas*/
+/*CONSULTA 7: Crear una vista que muestre al top 10 de países con mayor cantidad de pruebas*/
 	CREATE VIEW top_pais_mas_vacunas
 	AS
 	Select TOP 10 PRUEBA.ID_PAIS,PAIS.LOCATIONN,PRUEBA.TOTAL_TESTS, PRUEBA.DATEE from PRUEBA, PAIS WHERE PRUEBA.ID_PAIS=PAIS.ID_PAIS
